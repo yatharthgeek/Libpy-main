c0f7d58a0f85d8fdcf7ee67eb858762aa13afa91
yatharthgeek <81706493+yatharthgeek@users.noreply.github.com>
2022-03-20 10:11:16 -0700

    Add files via upload

---
+++ libpy-ch01.py
@@ -0,0 +1,52 @@
+# creating first program
+
+print("Hello world")
+
+# printing more than one strings 
+
+print("My Name is Rocket")
+print("Nice to meet you ")
+
+# printing using one print function
+
+print("And My name is Groot\n I Am Groot") # \n is used to break lines 
+
+# store a string inside a variable 
+
+var = "Hello"
+
+print (var)
+
+# taking inputs 
+
+input()
+
+# taking inputs more freshly
+
+print("what is your name ? ")
+input()
+
+# taking inputs in a variable
+
+var_input = input(" Input something in Strings ")
+
+# taking inputs as number
+
+var_input_num = int(input ("Input Something in numbers "))
+
+# adding two strings 
+
+var_input_add = input ("Enter your Name ")
+
+var_input_add2 = input ("Enter your Friend Name ")
+
+string_add = "Hello my name is "+var_input_add+ " and meet my friend "+var_input_add2 
+
+print(string_add)
+
+# Assignment 01 (Easy)
+# Find sum of two numbers 
+
+# Assignment 02 (Advanced)
+# create a Shop billing system accept the name and add two product purchase and release the final bill 
+
