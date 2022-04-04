3420ff2dd016fdf0d57e86a913e9ae1089e56906
yatharthgeek <81706493+yatharthgeek@users.noreply.github.com>
2022-04-05 00:04:23 +0530

    Create libpy-ch02.py

---
+++ libpy-ch02.py
@@ -0,0 +1,60 @@
+# Adding two numbers 
+
+num_1 = 55
+
+num_2 = 110
+
+print (num_1 + num_2)
+
+# Assignment 01
+
+# Take two numbers as input and add them
+
+# Subtracting two numbers
+
+num_1 = 555
+
+num_2 = 110
+
+print (num_1 - num_2)
+
+# Multiplying two numbers 
+
+num_1 = 55
+
+num_2 = 110
+
+print (num_1 * num_2)
+
+# Dividing two numbers 
+
+num_1 = 55
+
+num_2 = 110
+
+print (num_1 / num_2)
+
+# Using Multiple Mathamatical function at once 
+
+func_1 = "2+2*3+8-2+88-12/5"
+
+ans_1 = eval(func_1) # eval() is a python mathamatical function used to solve various mathamatical function at once 
+
+print(ans_1)
+
+#Assignment 02
+
+# Make a Calculator that takes question as input and gives answer as output 
+
+# Adding two strings 
+
+str_1 = "Hello my name is "
+
+str_2 = "Groot"
+
+print (str_1 + str_2 ) # When you are adding two or more strings these are called Concatenation 
+
+# Assignment 03
+
+# Make a Coffee shop and take name of people and their product as entry and then print it like eg. (Hello Mr.Tom , This is your Cold Coffee . Enjoy !! )
+
